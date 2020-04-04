5527785cbcd86e92fac5a44ae45a59f3917a4a78
伏特酱 <25198721+voltachan@users.noreply.github.com>
2020-04-04 13:54:29 +0800

    Create colorful.user.js

---
+++ colorful.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         我要彩色
+// @version      0.1
+// @description  Color back
+// @author       You
+// @match        http*://*/*
+// @grant        GM_addStyle
+// @run-at       document-start
+
+
+// ==/UserScript==
+
+(function() {
+    GM_addStyle("*{filter:grayscale(0) !important;}");
+    })();
