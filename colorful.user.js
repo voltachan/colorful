3a178a95102264bce8ce4db38ac71f6b623b69fc
伏特酱 <25198721+voltachan@users.noreply.github.com>
2020-04-04 14:10:02 +0800

    Update colorful.user.js

--- colorful.user.js
+++ colorful.user.js
@@ -11,5 +11,5 @@
 
 (function() {
     GM_addStyle("* {filter: none !important}");
-    GM_addStyle("* {-webkit-filter:grayscale(0)! important;-moz-filter:grayscale(0) !important;-ms-filter:grayscale(0) !important;-o-filter:grayscale(0) !important;filter:grayscale(0) !important;filter:none !important;}");
+    //GM_addStyle("* {-webkit-filter:grayscale(0)! important;-moz-filter:grayscale(0) !important;-ms-filter:grayscale(0) !important;-o-filter:grayscale(0) !important;filter:grayscale(0) !important;filter:none !important;}");
 })();
